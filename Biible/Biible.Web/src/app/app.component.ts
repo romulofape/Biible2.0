248763606fb20096fbdc0fe732606a7e5c6c1590
Romulo Pereira <romulo.fape@gmail.com>
2018-12-19 13:35:35 -0200

    Inclusão template
    Inclusão template

--- Biible/Biible.Web/src/app/app.component.ts
+++ Biible/Biible.Web/src/app/app.component.ts
@@ -1,27 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from './_services/auth.service';
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { User } from './_models/user';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  moduleId: module.id,
+  selector: 'app',
+  template: `<router-outlet></router-outlet>`
 })
-export class AppComponent implements OnInit {
-  jwtHelper = new JwtHelperService();
+export class AppComponent {
 
-  constructor(private authService: AuthService) {}
-
-  ngOnInit() {
-    const token = localStorage.getItem('token');
-    const user: User = JSON.parse(localStorage.getItem('user'));
-    if (token) {
-      this.authService.decodedToken = this.jwtHelper.decodeToken(token);
-    }
-    if (user) {
-      this.authService.currentUser = user;
-      this.authService.changeMemberPhoto(user.photoUrl);
-    }
-  }
 }
--- Biible/Biible.Web/src/app/app.module.ts
+++ Biible/Biible.Web/src/app/app.module.ts
@@ -1,93 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule }                    from '@angular/platform-browser';
+import { RouterModule }                     from '@angular/router';
+import { NgModule }                         from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BsDropdownModule, TabsModule, BsDatepickerModule, PaginationModule, ButtonsModule } from 'ngx-bootstrap';
-import { RouterModule } from '@angular/router';
-import { JwtModule } from '@auth0/angular-jwt';
-import { NgxGalleryModule } from 'ngx-gallery';
-import { FileUploadModule } from 'ng2-file-upload';
-import {TimeAgoPipe} from 'time-ago-pipe';
+import { BrowserAnimationsModule }          from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { AuthService } from './_services/auth.service';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
-import { AlertifyService } from './_services/alertify.service';
-import { MemberListComponent } from './members/member-list/member-list.component';
-import { ListsComponent } from './lists/lists.component';
-import { MessagesComponent } from './messages/messages.component';
-import { appRoutes } from './routes';
-import { AuthGuard } from './_guards/auth.guard';
-import { UserService } from './_services/user.service';
-import { MemberCardComponent } from './members/member-card/member-card.component';
-import { MemberDetailComponent } from './members/member-detail/member-detail.component';
-import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
-import { MemberListResolver } from './_resolvers/member-list.resolver';
-import { MemberEditComponent } from './members/member-edit/member-edit.component';
-import { MemberEditResolver } from './_resolvers/member-edit.resolver';
-import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
-import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
-import { ListsResolver } from './_resolvers/lists.resolver';
-import { MessagesResolver } from './_resolvers/messages.resolver';
-import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
+import { routes, AppRoutingModule }         from './app-routing.module';
+import { AppComponent }                     from './app.component';
+import { UIModule }                         from './ui/ui.module';
+import { NiComponentsModule }               from './ni-components/ni-components.module';
+import { PagesModule }                      from './pages/pages.module';
 
-export function tokenGetter() {
-  return localStorage.getItem('token');
-}
+import { DefaultLayoutComponent }           from './layouts/default/default.component';
+import { BoxedLayoutComponent }             from './layouts/boxed/boxed.component';
+import { DefaultCLayoutComponent }          from './layouts/default-c/default-c.component';
+import { BoxedCLayoutComponent }            from './layouts/boxed-c/boxed-c.component';
+import { ExtraLayoutComponent }             from './layouts/extra/extra.component';
 
 @NgModule({
-  declarations: [
+  declarations : [
     AppComponent,
-    NavComponent,
-    HomeComponent,
-    RegisterComponent,
-    MemberListComponent,
-    ListsComponent,
-    MessagesComponent,
-    MemberCardComponent,
-    MemberDetailComponent,
-    MemberEditComponent,
-    PhotoEditorComponent,
-    TimeAgoPipe,
-    MemberMessagesComponent
+    DefaultLayoutComponent,
+    BoxedLayoutComponent,
+    DefaultCLayoutComponent,
+    BoxedCLayoutComponent,
+    ExtraLayoutComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    BsDropdownModule.forRoot(),
-    BsDatepickerModule.forRoot(),
-    ButtonsModule.forRoot(),
-    PaginationModule.forRoot(),
-    TabsModule.forRoot(),
-    RouterModule.forRoot(appRoutes),
-    NgxGalleryModule,
-    FileUploadModule,
-    JwtModule.forRoot({
-      config: {
-        tokenGetter: tokenGetter,
-        whitelistedDomains: ['localhost:18029'],
-        blacklistedRoutes: ['localhost:18029/api/auth']
-      }
-    })
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes, { useHash: true }),
+
+    AppRoutingModule,
+    UIModule,
+    NiComponentsModule,
+    PagesModule
   ],
-  providers: [
-      AuthService,
-      ErrorInterceptorProvider,
-      AlertifyService,
-      AuthGuard,
-      UserService,
-      MemberDetailResolver,
-      MemberListResolver,
-      MemberEditResolver,
-      PreventUnsavedChanges,
-      ListsResolver,
-      MessagesResolver
-    ],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule {}
+
+export class AppModule { }
